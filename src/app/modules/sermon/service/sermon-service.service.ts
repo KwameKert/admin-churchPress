000e836718caa313f2e13d65402613b4007d2055
kwamekert <tomriddle477@gmail.com>
2020-03-28 19:56:32 +0000

    add sermon method added to service

--- src/app/modules/sermon/service/sermon-service.service.ts
+++ src/app/modules/sermon/service/sermon-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SermonServiceService {
 
-  constructor() { }
+  private _baseUrl :String = environment.api_host;
+  constructor(private _httpClient: HttpClient) { }
+
+
+  public addSermon(data: any){
+    return this._httpClient.post(`${this._baseUrl}/sermon/`, data);
+  }
+
 }
