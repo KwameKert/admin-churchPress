ee65bab15ee418e477d54453173f3bf5bc399e4d
kwamekert <tomriddle477@gmail.com>
2020-04-01 23:01:53 +0000

    Department routing wired up

--- src/app/modules/department/department-routing.module.ts
+++ src/app/modules/department/department-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { Routes, RouterModule } from '@angular/router';
+import { AddDepartmentComponent } from './components/add-department/add-department.component';
+import { ListDepartmentComponent } from './components/list-department/list-department.component';
+import { UpdateDepartmentComponent } from './components/update-department/update-department.component';
 
+const routes: Routes = [ 
+
+  {path: 'add', component: AddDepartmentComponent},
+  {path: 'list', component: ListDepartmentComponent},
+  {path: 'update/:id', component: UpdateDepartmentComponent},
+  
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  
 })
 export class DepartmentModule { }
