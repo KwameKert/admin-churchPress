8d65e675aff2e30678149d8effafd89656312604
kwamekert <tomriddle477@gmail.com>
2020-04-02 23:30:29 +0000

    Updates

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 
+
+
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -13,4 +15,6 @@ res.sendFile(path.join(__dirname+'/dist/hopepress/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
+
+console.log("i'm serving")
