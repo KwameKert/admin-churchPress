f8d5446c8968cae8716804b89d226e3faf57b5fb
kwamekert <tomriddle477@gmail.com>
2020-04-02 22:02:05 +0000

    Updated package.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/churchpress'));
+app.use(express.static(__dirname + '/dist/chruchPress-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./dist/churchpress/index.html'));
+res.sendFile(path.join(__dirname+'/dist/chruchPress-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
